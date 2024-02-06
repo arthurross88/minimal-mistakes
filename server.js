b98b209662a6443c74d9445fdf4e84e5749c9f9a
Arthur Ross <arthurross@live.nl>
2024-02-06 16:09:03 +0100

    cert

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 const port = 3000;
 
 const options = {
-  key: fs.readFileSync('keys/www.theeternalfireofthegods.com_key.txt'),
-  cert: fs.readFileSync('keys/www.theeternalfireofthegods.com.p7b'),
+  key: fs.readFileSync('keys/www.theeternalfireofthegods.com.crt'),
 };
 
 const server = https.createServer(options, app);
