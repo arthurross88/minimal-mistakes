c8386c5f390cf10b92c52e3c922b5d1f7cf62b97
Arthur Ross <arthurross@live.nl>
2024-02-07 10:51:43 +0100

    linter

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 const options = {
-  key: fs.readFileSync('keys/www.theeternalfireofthegods.com_key.txt'),
+  key: fs.readFileSync('/Users/mac/Desktop/www.theeternalfireofthegods.com_key.txt'),
   cert: fs.readFileSync('keys/www.theeternalfireofthegods.com.crt'),
 };
 
